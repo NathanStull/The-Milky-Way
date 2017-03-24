17780b89eb6e558af732b8c681f623eb30278472
ArcadeDev <ArcadeDev>
2017-03-24 08:24:15 -0700

    scrolling text

--- TestScene.js
+++ TestScene.js
@@ -1,14 +1,14 @@
 'use strict';
-var BLACK = '#000000';
+var BLACK = '#4286f4';
 
 function TestScene(image) {
   Scene.call(this);
-  this.img = new StaticImage(0, 0, windowWidth * 0.8, windowHeight * 0.75, image);
+  this.img = new StaticImage(0, 0, windowWidth, windowHeight, image);
   this.addActor(this.img);
   
-  this.textBox = new textBox(0, windowHeight* 0.75, windowWidth, windowHeight * 0.25, "I AM A TEST DEL WIT IT", {size:50, leading:50}, null, BLACK);
+  this.textBox = new textBox(0, windowHeight* 0.75, windowWidth, windowHeight * 0.25, "I AM A TEST DEL WIT IT", {size:50, leading:50, fill:RED, font:"Times New Roman"}, null, BLACK);
   this.addActor(this.textBox);
-  
+  this.textBox.updateText("This text is scrolling!!!!!\n It is working.");
   console.log("created Test Scene");
 }
 
--- textBox.js
+++ textBox.js
@@ -1,6 +1,8 @@
 'use strict';
 
 
+var letterArray;
+
 function textBox(x, y, width, height, text, textattrs, nudge, bgColor) {
   Actor.call(this, x, y, width, height);
   
@@ -20,12 +22,41 @@ textBox.prototype.draw = function() {
   Actor.prototype.draw.call(this);
 
   _applyTextAttrs(this.textattrs);
+  textFont("Georgia");
   text(this.text,
        0,
        0,
        this.width,
        this.height);
 };
+
+textBox.prototype.updateText = function(newText){
+  this.text = newText;
+  letterArray = this.text.split("");
+  this.text = "";
+  setTimeout(this.rollText.bind(this), 75, 0);
+  text(this.text,
+       0,
+       0,
+       this.width,
+       this.height);
+  
+}
+
+textBox.prototype.rollText = function(indexNum){
+  if(indexNum<=letterArray.length - 1){
+    this.text = this.text + letterArray[indexNum];
+    text(this.text,
+       0,
+       0,
+       this.width,
+       this.height);
+    indexNum++;
+    console.log("RollingText" + indexNum);
+    setTimeout(this.rollText.bind(this), 75, indexNum);
+  }
+}
+
 
 /* this.nudge[0] ,
    this.nudge[1] + this.height/2 - this.textattrs.size/2, */
